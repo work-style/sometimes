173ede6fc56a4905045da8d5844a5d732da492e7
苗大伟 <miaodawei@miaodaweideMBP.lan>
2019-08-20 23:37:21 +0800

    all

---
+++ davidblog/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miaodawei
+ * Date: 2019/8/20
+ * Time: 11:27 PM
+ */
+
+namespace davidblog\controllers;
+
+
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+
+}
